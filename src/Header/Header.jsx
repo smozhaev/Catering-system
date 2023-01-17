9e664740616ca32281412a41e458d9ab7f0849df
smozhaev <kmozhaev100@gmail.com>
2023-01-17 20:00:49 +0300

    0.3.2v commit

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => {
     return(
         <header className="header" >
-            <h1>Вкусняшка</h1>
+            <h1>Вкусняш</h1>
             <ul className='ul-header'>
                 <li className='li-header'><a href='/'>Главная</a></li>
                 <li className='li-header'><a href='/basket'>Корзина</a></li>
