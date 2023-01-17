f536913f87e3413658a989284d2b593afe24c5fa
thanhthuynguyen89 <natasha.nguyen.089@gmail.com>
2023-01-17 19:57:34 +0300

    css2.0 commit2

--- src/Main/drinks/Drinks.jsx
+++ src/Main/drinks/Drinks.jsx
@@ -5,13 +5,13 @@ const Drinks = () => {
     return(
         <div className="drink-block">
             <a className='link-drink' href='/'>назад</a>
-            <h1>Drinks</h1>
+            <h1 className='title-drinks'>Drinks</h1>
             <ul className="ul-drink">
-                <li className="li-drink"><a href='/drinks/tea'>Чай</a></li>
-                <li className="li-drink"><a href='/hot'>Кофе</a></li>
-                <li className="li-drink"><a href='/drinks'>Вода</a></li>
-                <li className="li-drink"><a href='/drinks'>Энэргетики</a></li>
-                <li className="li-drink"><a href='/drinks'>Газировка</a></li>
+                <li className="li-drink"><a className="a-drink" href='/drinks/tea'>Чай</a></li>
+                <li className="li-drink"><a className="a-drink" href='/hot'>Кофе</a></li>
+                <li className="li-drink"><a className="a-drink" href='/drinks'>Вода</a></li>
+                <li className="li-drink"><a className="a-drink" href='/drinks'>Энэргетики</a></li>
+                <li className="li-drink"><a className="a-drink" href='/drinks'>Газировка</a></li>
             </ul>
         </div>
     )
